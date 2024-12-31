671edd8b06a32ee477acc9565abe3540dfd38aa0
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2024-12-30 20:36:27 -0800

    Time: 0 ms (100.00%) | Memory: 13.4 MB (8.30%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,15 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # create a dictionary
         dict_nums = {}
-        # Use enumerate method to add to the dictionary
-        for i, num in enumerate(nums):
-            dict_nums[num] = i
-        # Use for loop to check for the subtraction 
-        for i, num in enumerate(nums):
-            diff = target - num
+        for i,v in enumerate(nums):
+            dict_nums[v] = i
+        for i in range(len(nums)):
+            diff = target - nums[i]
             if diff in dict_nums and dict_nums[diff] != i:
                 return [i, dict_nums[diff]]
-        
-        
