21465b59a6e174b57cd34a5c06178fd11c2b1b04
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2024-12-31 17:26:09 -0800

    Time: 60 ms (5.33%) | Memory: 13.5 MB (5.80%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,16 +5,33 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        left = 0
-        right = len(numbers) - 1 
-        while left < right:
-            currSum = numbers[left] + numbers[right]
-            if currSum > target:
-                right -= 1
-            elif currSum < target:
-                left += 1
-            else:
-                return [left + 1, right + 1]
+        for i in range(len(numbers)):
+            l, r = i + 1, len(numbers) - 1
+            diff = target - numbers[i]
+            while l <= r:
+                mid = l + (r - l) // 2
+                if numbers[mid] == diff:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < diff:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return []
+            
+                    
+
+
+
+        # left = 0
+        # right = len(numbers) - 1 
+        # while left < right:
+        #     currSum = numbers[left] + numbers[right]
+        #     if currSum > target:
+        #         right -= 1
+        #     elif currSum < target:
+        #         left += 1
+        #     else:
+        #         return [left + 1, right + 1]
         
             
         
