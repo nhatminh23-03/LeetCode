a187d09f770fe63582570003c7433ec481ac9bfe
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-01-03 15:57:53 -0800

    Time: 11 ms (40.35%) | Memory: 13.8 MB (10.09%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -4,24 +4,40 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
+        # stack = []
+        # for token in tokens:
+        #     if token.isdigit() or (token[0] == '-' and len(token) > 1):
+        #         stack.append(int(token))
+        #     else:
+        #         if len(stack) < 2:
+        #             raise ValueError("Insufficient operands for operator")
+        #         val1 = stack.pop()
+        #         val2 = stack.pop()
+
+        #         if token == "+":
+        #             stack.append(val2 + val1)
+        #         elif token == "-":
+        #             stack.append(val2 - val1)
+        #         elif token == "*":
+        #             stack.append(val2 * val1)
+        #         elif token == "/":
+        #             stack.append(int(float(val2) / val1))  # Integer division
+        # return stack[0]
+
         stack = []
         for token in tokens:
-            if token.isdigit() or (token[0] == '-' and len(token) > 1):
-                stack.append(int(token))
+            if token == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif token == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif token == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
             else:
-                if len(stack) < 2:
-                    raise ValueError("Insufficient operands for operator")
-                val1 = stack.pop()
-                val2 = stack.pop()
-
-                if token == "+":
-                    stack.append(val2 + val1)
-                elif token == "-":
-                    stack.append(val2 - val1)
-                elif token == "*":
-                    stack.append(val2 * val1)
-                elif token == "/":
-                    stack.append(int(float(val2) / val1))  # Integer division
+                stack.append(int(token))
         return stack[0]
 
 
