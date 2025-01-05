0ed1925048c7ea9984ae92dec07b1f91298efe0b
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-01-05 12:08:59 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.19%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
+        dict_c = {")": "(", "}": "{", "]": "["}
         for c in s:
-            if c == "(" or c == "{" or c == "[":
-                stack.append(c)
-            else:
-                if not stack:
+            if c in dict_c:
+                if stack and stack[-1] == dict_c[c]:
+                    stack.pop()
+                else:
                     return False
-                topStack = stack.pop()
-                if (c == ")" and not topStack == "(") or (c == "]" and not topStack == "[") or (c == "}" and not topStack == "{"):
-                    return False 
+            else:
+                stack.append(c)
         return len(stack) == 0
-                    
+        
