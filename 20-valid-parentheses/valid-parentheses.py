4304d67778fd24dc8c39a27a220aaabf0b76c32a
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-01-05 12:13:24 -0800

    Time: 2 ms (59.59%) | Memory: 12.6 MB (6.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # stack = []
+        # dict_c = {")": "(", "}": "{", "]": "["}
+        # for c in s:
+        #     if c in dict_c:
+        #         if stack and stack[-1] == dict_c[c]:
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(c)
+        # return len(stack) == 0
         stack = []
-        dict_c = {")": "(", "}": "{", "]": "["}
         for c in s:
-            if c in dict_c:
-                if stack and stack[-1] == dict_c[c]:
-                    stack.pop()
-                else:
-                    return False
-            else:
+            if c == "{" or c == "(" or c == "[":
                 stack.append(c)
+            else:
+                if not stack:
+                    return False
+                topStack = stack.pop()
+                if (c == "}" and topStack != "{") or (c == ")" and topStack != "(") or (c == "]" and topStack != "["):
+                    return False
         return len(stack) == 0
         
