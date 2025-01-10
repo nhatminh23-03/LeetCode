a368b0dc0abbd80c04eb085490291f41d3105641
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-01-10 15:19:33 -0800

    Time: 107 ms (36.72%) | Memory: 19 MB (44.18%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # indexBuy = 0
+        # maxProfit = 0 
+        # for i in range(len(prices)):
+        #     if prices[i] < prices[indexBuy]:
+        #         indexBuy = i
+        #     maxProfit = max(maxProfit, prices[i] - prices[indexBuy])
+        # return maxProfit 
+
+        # Or using 2 pointer technique 
+        maxProfit = 0 
+        l, r = 0, 1
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+            else:
+                l = r  
+            r += 1
+        return maxProfit
+
+            
