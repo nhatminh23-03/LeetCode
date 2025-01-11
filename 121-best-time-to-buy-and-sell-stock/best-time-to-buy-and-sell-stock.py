b8b82e2d049c686a275f3a24bedc61f170c3aaaa
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-01-11 13:43:11 -0800

    Time: 103 ms (39.85%) | Memory: 19.5 MB (7.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,23 +4,46 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # indexBuy = 0
-        # maxProfit = 0 
-        # for i in range(len(prices)):
-        #     if prices[i] < prices[indexBuy]:
-        #         indexBuy = i
-        #     maxProfit = max(maxProfit, prices[i] - prices[indexBuy])
-        # return maxProfit 
-
-        # Or using 2 pointer technique 
+        # Using for loop
+        indexBuy = 0
         maxProfit = 0 
-        l, r = 0, 1
-        while r < len(prices):
-            if prices[r] > prices[l]:
-                maxProfit = max(maxProfit, prices[r] - prices[l])
+        for i in range(len(prices)):
+            if prices[i] > prices[indexBuy]:
+                maxProfit = max(maxProfit, prices[i] - prices[indexBuy])
             else:
-                l = r  
-            r += 1
+                indexBuy = i
         return maxProfit
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # indexBuy = 0
+        # # maxProfit = 0 
+        # # for i in range(len(prices)):
+        # #     if prices[i] < prices[indexBuy]:
+        # #         indexBuy = i
+        # #     maxProfit = max(maxProfit, prices[i] - prices[indexBuy])
+        # # return maxProfit 
+
+        # # Or using 2 pointer technique 
+        # maxProfit = 0 
+        # l, r = 0, 1
+        # while r < len(prices):
+        #     if prices[r] > prices[l]:
+        #         maxProfit = max(maxProfit, prices[r] - prices[l])
+        #     else:
+        #         l = r  
+        #     r += 1
+        # return maxProfit
 
             
