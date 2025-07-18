9a461a32764ee3ab2cc47b6fac58e8a756b65faa
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-07-18 14:49:22 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (47.78%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,44 +6,30 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-            # if not root:
-            #     return 0
-            
-            # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # if not root:
+        #     return 0
+        
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-            # BFS traversal 
+        # DFS 
+        if not root:
+            return 0
 
-            # if not root:
-            #     return 0
-            
-            # level = 0
-            # queue = deque([root])
+        stack = [[root, 1]]
 
-            # while queue:
-            #     for i in range(len(queue)):
-            #         node = queue.popleft()
-            #         if node.left:
-            #             queue.append(node.left)
-            #         if node.right:
-            #             queue.append(node.right)
-            #     level += 1 
-                
-            # return level
+        level = 0
 
-            # DFS Traversal
+        while stack:
+            node, depth = stack.pop()
 
-            if not root:
-                return 0
-            
-            stack = [[root, 1]]
-            level = 1 
+            if node:
+                level = max(level, depth)
+                stack.append([node.right, depth + 1])
+                stack.append([node.left, depth + 1])
+        
+        return level
 
-            while stack:
-                node, depth = stack.pop()
 
-                if node:
-                    level = max(level, depth)
-                    stack.append([node.left, 1 + depth])
-                    stack.append([node.right, 1 + depth])
-            return level
+
+        
         
