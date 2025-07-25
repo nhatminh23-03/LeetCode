5c038fc3380fc1ab1e55b23d05e96a1c165b6a1b
Minh Nhat Doan <66338703+nhatminh23-03@users.noreply.github.com>
2025-07-25 15:26:45 -0700

    Time: 168 ms (25.04%) | Memory: 78 MB (87.22%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,7 +1,7 @@
-class Node:
+class ListNode():
     def __init__(self, key, val):
         self.key, self.val = key, val
-        self.prev = self.next = None
+        self.prev, self.next = None, None
 
 class LRUCache:
 
@@ -9,36 +9,39 @@ class LRUCache:
         self.cap = capacity
         self.cache = {}
 
-        self.left , self.right = Node(0,0), Node(0,0)
+        self.left, self.right = ListNode(0,0), ListNode(0,0)
         self.left.next, self.right.prev = self.right, self.left
     
     def remove(self, node):
         prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
+        prev.next = nxt
+        nxt.prev = prev
 
     def add(self, node):
         prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev  = self.right, prev
+        prev.next = nxt.prev = node 
+        node.prev, node.next = prev, nxt
 
+        
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.add(self.cache[key])
             return self.cache[key].val
-        return -1 
+        return -1
         
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         
-        self.cache[key] = Node(key, value)
+        self.cache[key] = ListNode(key, value)
         self.add(self.cache[key])
 
         if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
